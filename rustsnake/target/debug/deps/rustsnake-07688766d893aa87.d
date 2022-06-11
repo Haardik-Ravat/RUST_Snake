2f9c65f0e2b1a607a221e1e7c12cd46bcb57b28d
Haardik <ravat.1@iitj.ac.in>
2022-06-11 21:54:46 +0530

    initialization

---
+++ rustsnake/target/debug/deps/rustsnake-07688766d893aa87.d
@@ -0,0 +1,5 @@
+D:\GitHub\RUST_Snake\rustsnake\target\debug\deps\rustsnake-07688766d893aa87.exe: src\main.rs
+
+D:\GitHub\RUST_Snake\rustsnake\target\debug\deps\rustsnake-07688766d893aa87.d: src\main.rs
+
+src\main.rs:
---
+++ rustsnake/target/debug/rustsnake.d
@@ -0,0 +1,1 @@
+D:\GitHub\RUST_Snake\rustsnake\target\debug\rustsnake.exe: D:\GitHub\RUST_Snake\rustsnake\src\main.rs
